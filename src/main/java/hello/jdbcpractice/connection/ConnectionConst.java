c351172aa9a863f00a1c310fa5a9851ea4bf69cf
kimkyumbi <kimgyeombi@naver.com>
2024-07-25 19:13:07 +0900

    JDBC

---
+++ src/main/java/hello/jdbcpractice/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbcpractice.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USER = "sa";
+    public static final String PASSWORD = "";
+}
