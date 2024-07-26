84532bf55b5e28488e19e3fbfd707566ab7ac2d3
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 15:01:24 +0900

    Transaction

---
+++ src/main/java/hello/jdbcpractice/service/MemberServiceV2.java
@@ -0,0 +1,58 @@
+package hello.jdbcpractice.service;
+
+import hello.jdbcpractice.domain.member.Member;
+import hello.jdbcpractice.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 repository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            bizLogic(connection, fromId, toId, money);
+            connection.commit();
+        } catch (Exception e) {
+            connection.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(connection);
+        }
+    }
+
+    private void bizLogic(Connection connection, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = repository.findById(connection, fromId);
+        Member toMember = repository.findById(connection, toId);
+
+        repository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        repository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (Exception e) {
+                log.info("error : ", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 오류 발생");
+        }
+    }
+}
