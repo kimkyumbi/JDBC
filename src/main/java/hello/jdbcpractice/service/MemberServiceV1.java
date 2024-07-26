c5732ba2af4d7566725dcd9d155cace5da7437df
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 10:56:42 +0900

    Transaction

---
+++ src/main/java/hello/jdbcpractice/service/MemberServiceV1.java
@@ -0,0 +1,27 @@
+package hello.jdbcpractice.service;
+
+import hello.jdbcpractice.domain.member.Member;
+import hello.jdbcpractice.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+    private final MemberRepositoryV1 repository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = repository.findById(fromId);
+        Member toMember = repository.findById(toId);
+
+        repository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        repository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 오류 발생");
+        }
+    }
+}
