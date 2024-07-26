b3968916a449910c885a681eb5fac6f23ae8812c
kimkyumbi <kimgyeombi@naver.com>
2024-07-26 15:01:26 +0900

    Transaction

---
+++ src/test/java/hello/jdbcpractice/service/MemberServiceV2Test.java
@@ -0,0 +1,82 @@
+package hello.jdbcpractice.service;
+
+import hello.jdbcpractice.domain.member.Member;
+import hello.jdbcpractice.repository.MemberRepositoryV1;
+import hello.jdbcpractice.repository.MemberRepositoryV2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static hello.jdbcpractice.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberServiceV2Test {
+    private static final String MEMBER_A = "memberA";
+    private static final String MEMBER_B = "memberB";
+    private static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV2 memberRepositoryV2;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USER, PASSWORD);
+        memberRepositoryV2 = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepositoryV2);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepositoryV2.delete(MEMBER_A);
+        memberRepositoryV2.delete(MEMBER_B);
+        memberRepositoryV2.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepositoryV2.save(memberA);
+        memberRepositoryV2.save(memberB);
+
+        // when
+        log.info("START TX");
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.info("END  TX");
+
+        // then
+        Member findMemberA = memberRepositoryV2.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositoryV2.findById(memberB.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생 ")
+    void accountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepositoryV2.save(memberA);
+        memberRepositoryV2.save(memberEx);
+
+        // when
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findMemberA = memberRepositoryV2.findById(memberA.getMemberId());
+        Member findMemberB = memberRepositoryV2.findById(memberEx.getMemberId());
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
+    }
+}
