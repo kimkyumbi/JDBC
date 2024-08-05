2d8db839f4ba483fbad2e112549e79d8766e1690
kimkyumbi <kimgyeombi@naver.com>
2024-08-05 12:14:00 +0900

    DB

---
+++ src/test/java/hello/jdbcpractice/exception/basic/UnCheckedAppTest.java
@@ -0,0 +1,80 @@
+package hello.jdbcpractice.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+@Slf4j
+public class UnCheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(controller::request)
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+//            e.printStackTrace();
+            log.info("ex", e);
+        }
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() {
+            service.lozic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetWorkClient netWorkClient = new NetWorkClient();
+
+        public void lozic() {
+            netWorkClient.call();
+            repository.call();
+        }
+    }
+
+    static class NetWorkClient {
+        public void call() {
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        private void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
