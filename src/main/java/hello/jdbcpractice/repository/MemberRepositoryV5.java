7f5becb793fdcd7c09d6a70734492fb63d1a169f
kimkyumbi <kimgyeombi@naver.com>
2024-08-05 12:13:16 +0900

    DB

---
+++ src/main/java/hello/jdbcpractice/repository/MemberRepositoryV5.java
@@ -0,0 +1,63 @@
+package hello.jdbcpractice.repository;
+
+import hello.jdbcpractice.connection.DBConnectionUtil;
+import hello.jdbcpractice.domain.member.Member;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * JDBC 템플릿 사용
+ */
+@Slf4j
+public class MemberRepositoryV5 implements MemberRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemberRepositoryV5(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member (member_id, money) values (?, ?)";
+        jdbcTemplate.update(sql, member.getMemberId(), member.getMoney());
+        return member;
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+        return jdbcTemplate.queryForObject(sql, memberRowMapper(), memberId);
+    }
+
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+        jdbcTemplate.update(sql, money, memberId);
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id = ?";
+        jdbcTemplate.update(sql, memberId);
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return  (rs, rowNum) -> {
+            Member member = new Member();
+            member.setMemberId(rs.getString("member_id"));
+            member.setMoney(rs.getInt("money"));
+            return member;
+        };
+    }
+}
