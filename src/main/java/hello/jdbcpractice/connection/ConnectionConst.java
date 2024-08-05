39e01d9c085e76f3b094b0b999c0146f55028902
kimkyumbi <kimgyeombi@naver.com>
2024-08-05 12:12:33 +0900

    Add ConnectionConst.java

--- src/main/java/hello/jdbcpractice/connection/ConnectionConst.java
+++ src/main/java/hello/jdbcpractice/connection/ConnectionConst.java
@@ -1,6 +1,7 @@
 package hello.jdbcpractice.connection;
 
 public abstract class ConnectionConst {
+    // H2 데이터베이스 연결 관련 정보
     public static final String URL = "jdbc:h2:tcp://localhost/~/test";
     public static final String USER = "sa";
     public static final String PASSWORD = "";
