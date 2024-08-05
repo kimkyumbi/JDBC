ade8449e6e4c3322894e2d891beb473d9303d7d1
kimkyumbi <kimgyeombi@naver.com>
2024-08-05 12:12:47 +0900

    DB

--- src/main/java/hello/jdbcpractice/domain/member/Member.java
+++ src/main/java/hello/jdbcpractice/domain/member/Member.java
@@ -8,9 +8,7 @@ public class Member {
     private String memberId;
     private int money;
 
-    public Member() {
-
-    }
+    public Member() { }
 
     public Member(String memberId, int money) {
         this.memberId = memberId;
