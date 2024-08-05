00fd4ea8471eee0b580538da9bbd7bf07eabea26
kimkyumbi <kimgyeombi@naver.com>
2024-08-05 12:12:44 +0900

    DB

--- src/main/java/hello/jdbcpractice/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbcpractice/connection/DBConnectionUtil.java
@@ -10,8 +10,13 @@ import static hello.jdbcpractice.connection.ConnectionConst.*;
 
 @Slf4j
 public class DBConnectionUtil {
+    // Connection 생성 로직
     public static Connection getConnection() {
         try {
+            /**
+             * DriverManager의 getConnection 메서드에
+             * ConnectionConst에서 H2 데이터베이스의 URL, USER, PASSWORD 정보를 넣어서 직접 커넥션 생성
+             */
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
             log.info("get connectin={} class={}", connection, connection.getClass());
             return connection;
